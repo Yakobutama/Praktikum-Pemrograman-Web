0fac46dc5d96145d64011f8cd222bcc59a62ee7e
Yakobutama <118521801+Yakobutama@users.noreply.github.com>
2022-11-18 21:09:01 +0700

    Add files via upload
    push

---
+++ yakob utama (2170231054) pemograman web/pertemuan 4/latihan2.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>pemrograman php dengan array</title>
+</head>
+<body>
+<?php
+//penulisan array dapat dibuat seperti berikut
+$nama[] = "Yakob utama";
+$nama[] = "rizki";
+$nama[] = "rijal";
+echo $nama[1] . $nama[2] . $nama[0];
+echo "<br>";
+?>
+</body>
+</html>
+    
---
+++ yakob utama (2170231054) pemograman web/pertemuan 4/latihan8.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <meta http-equiv="refresh" content="2; url=http://localhost/yakob utama (2170231054) pemograman web/.html">
+</head>
+<Body>
+    Test Redirect
+</body>
+</html>
